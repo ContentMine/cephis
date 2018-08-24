3a51c1dd9dbd345befca00ce0ffe8b0cf7dd2a14
Peter Murray-Rust <peter.murray.rust@googlemail.com>
2018-08-24 14:31:08 +0100

    added bioRxiv functionality and fixed appendRectangle problem

---
+++ src/test/java/org/contentmine/pdf2svg2/Biorxiv.java
@@ -0,0 +1,84 @@
+package org.contentmine.pdf2svg2;
+
+import java.io.File;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.contentmine.cproject.files.CProject;
+import org.contentmine.cproject.util.CMineTestFixtures;
+import org.contentmine.graphics.svg.SVGHTMLFixtures;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class Biorxiv {
+	private static final Logger LOG = Logger.getLogger(Biorxiv.class);
+	static {
+		LOG.setLevel(Level.DEBUG);
+	}
+
+	@Test
+	public void testBiorxivMarchantia1() throws Exception {
+		File sourceDir = SVGHTMLFixtures.BIORXIV_DIR;
+		File targetDir = SVGHTMLFixtures.BIORXIV_TARGET_DIR;
+		CMineTestFixtures.cleanAndCopyDir(sourceDir, targetDir);
+		String[] files = {
+		"103861",
+		"188912",
+		"199992",
+		"220731",
+		"247593",
+		"277350",
+		"329839",
+		"362400",
+		"363937",
+		"385682"
+	};
+		String fileroot = "103861";
+		File file = new File(targetDir, fileroot+".full.pdf");
+		Assert.assertTrue("target pdf "+targetDir, file.exists());
+	    PDFDocumentProcessor documentProcessor = new PDFDocumentProcessor();
+	    documentProcessor.addExcludePages(10);
+	    documentProcessor.readAndProcess(file);
+	    documentProcessor.writeSVGPages(new File(targetDir, fileroot));
+	    try {
+	    	documentProcessor.writeRawImages(targetDir);
+	    } catch (Exception e) {
+	    	LOG.error("image creaton NYI");
+	    }
+	}
+
+	@Test
+	public void testBiorxivMarchantiaAll() throws Exception {
+		File sourceDir = SVGHTMLFixtures.BIORXIV_DIR;
+		File targetDir = SVGHTMLFixtures.BIORXIV_TARGET_DIR;
+		CMineTestFixtures.cleanAndCopyDir(sourceDir, targetDir);
+		String[] files = {
+		"103861",
+		"188912",
+		"199992",
+//		"220731", // this one is long and complex
+		"247593",
+		"277350",
+		"329839",
+		"362400",
+		"363937",
+		"385682"
+	};
+		for (String fileroot : files) {
+			File file = new File(targetDir, fileroot+".full.pdf");
+			LOG.debug("******* "+file+" **********");
+			Assert.assertTrue("target pdf "+targetDir, file.exists());
+		    PDFDocumentProcessor documentProcessor = new PDFDocumentProcessor();
+		    documentProcessor.addExcludePages(10);
+		    documentProcessor.readAndProcess(file);
+		    File outputDir = new File(targetDir, fileroot);
+			documentProcessor.writeSVGPages(outputDir);
+		    try {
+		    	documentProcessor.writeRawImages(targetDir);
+		    } catch (Exception e) {
+		    	LOG.error("image creation NYI");
+		    }
+		}
+	}
+}
