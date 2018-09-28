a3eaa49a07b001bb7f276f61766729ed8680d231
Peter Murray-Rust <peter.murray.rust@googlemail.com>
2018-09-28 19:01:43 +0100

    added schema verification of CProjects

---
+++ src/test/java/org/contentmine/cproject/files/CProjectCheckTest.java
@@ -0,0 +1,22 @@
+package org.contentmine.cproject.files;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+/** CProjectCheck checks actual files and directories agains a schema.
+ * 
+ * @author pm286
+ *
+ */
+public class CProjectCheckTest {
+	private static final Logger LOG = Logger.getLogger(CProjectCheckTest.class);
+	static {
+		LOG.setLevel(Level.DEBUG);
+	}
+
+	@Test
+	public void testCheck() {
+		
+	}
+}
