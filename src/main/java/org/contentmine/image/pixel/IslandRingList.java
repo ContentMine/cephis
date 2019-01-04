a33f8779a79e576d85b9714a1a1380a8b69af3aa
Peter Murray-Rust <peter.murray.rust@googlemail.com>
2019-01-04 11:24:19 +0000

    add IslandRingList to manage disconnected pixel rings

---
+++ src/main/java/org/contentmine/image/pixel/IslandRingList.java
@@ -0,0 +1,10 @@
+package org.contentmine.image.pixel;
+
+/** manages island subrings within a single PixelRing
+ * 
+ * @author pm286
+ *
+ */
+public class IslandRingList extends PixelRingList {
+
+}
